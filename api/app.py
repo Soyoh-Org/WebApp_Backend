44fad529240bc49a8bef8b9656020278aa1f721b
Francisco Verano Hoyos <franveho@gmail.com>
2020-01-25 15:12:39 -0600

    Added flask to requirements.txt and minor changes.

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return '<h1>Web API</h1><p>This site is a prototype API.</p>'
 
-app.run()
+
+app.run(host='0.0.0.0')
