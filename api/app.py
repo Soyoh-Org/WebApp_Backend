fbf754bf5133a0100e7df6cd70842f356712acbd
Francisco Verano Hoyos <franveho@gmail.com>
2020-01-25 20:57:56 -0600

    Added mock requests for /user route (GET, POST, DELETE).

--- api/app.py
+++ api/app.py
@@ -16,29 +16,59 @@ def home():
 
 @app.route('/users', methods=['GET'])
 def users():
-    users_data = read_json()
+    users_data = read_json_file()
     return users_data
 
 
 @app.route('/user', methods=['GET', 'POST', 'DELETE'])
-def user_by_id():
+def mock_user_requests():
     if request.method == 'GET':
-        json_data = read_json()
-
         if 'id' in request.args and request.args['id']:
             user_id = request.args['id']
         else:
-            return 'Error: No id field provided. Please specify an id.', 404
+            return '<p>Error: No id field provided. Please specify an id.</p>', 404
+
+        json_data = read_json_file()
 
         for x in json_data['users']:
             if x['id'] == user_id:
                 return jsonify(x)
+
         return '<p>There was a problem the request.</p>', 404
 
+    if request.method == 'POST':
+        data = request.json
+        write_json_file(data)
+        return '<p>The user was successfully added.</p>', 201
 
-def read_json():
+    if request.method == 'DELETE':
+        if 'id' in request.args and request.args['id']:
+            user_id = request.args['id']
+        else:
+            return '<p>Error: No id field provided. Please specify an id.</p>', 404
+
+        json_data = read_json_file()
+
+        for element in json_data['users']:
+            if user_id == element['id']:
+                json_data['users'].remove(element)
+
+        with open('mockdata/mock.json', 'w') as outfile:
+            json.dump(json_data, outfile, indent=4, sort_keys=True)
+        return '<p>The user with the id: {} was successfully deleted.</p>'.format(user_id), 200
+
+
+def read_json_file():
     with open('mockdata/mock.json') as json_file:
         return json.load(json_file)
+
+
+def write_json_file(data):
+    json_data = read_json_file()
+    json_data['users'].append(data)
+
+    with open('mockdata/mock.json', 'w') as outfile:
+        json.dump(json_data, outfile, indent=4, sort_keys=True)
 
 
 app.run(host='0.0.0.0')
